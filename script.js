0212e555b314e81a033897027ad26eb57d81fbe6
rhmatfurqon <rayjaproduction@gmail.com>
2025-08-22 15:32:47 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function handleOpenGame (data) {
-  const linkUrl = data.getAttribute("data-url")
-  window.open(linkUrl, "_blank")
-}
-
-function handleSearch(){
- const input = document.getElementById("search")
- input.classList.toggle("active")
-}
